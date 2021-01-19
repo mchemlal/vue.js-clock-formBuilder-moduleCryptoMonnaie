a8e6f160395d906cc331751fce5522f44315e009
YPariset <yoann.pariset@edu.itescia.fr>
2021-01-19 22:29:06 +0100

    button components added to formbuilder

--- src/components/FormBuilder.vue
+++ src/components/FormBuilder.vue
@@ -1,11 +1,24 @@
 <template>
     <div>
-      <div v-for="(item, index) in schema.fields" :key="index">
+        <el-card class="box-card" style="margin-right: 3px;">
+            <div slot="header" class="clearfix">
+                <span>Preview</span>
+            </div>
+        <div v-for="(item, index) in schema.fields" :key="index">
           <component :is="getCompo(item)"></component>
-      </div>
-</div>
+        </div>
+        </el-card>
+     <el-card class="box-card">
+         <div slot="header" class="clearfix">
+            <span>All Elements</span>
+         </div>
+         <!-- <div v-for="o in 4" :key="o" class="text item" > -->
+            <el-button v-on:click="addTextArea" >Add Text Area</el-button>
+            <el-button v-on:click="addBouttonRadio" >Add Button Radio </el-button>
+         <!-- </div> -->
+        </el-card>
+    </div>
 </template>
-
 
 <script>
 import InputTextArea from "./InputTextArea"
@@ -13,16 +26,60 @@ import RadioBox from "./RadioBox"
 export default {
     props: {
         schema: Object,
-        model: Object
+       
     },
     methods: {
+        addTextArea() {
+            this.schema.fields.push({inputType: "textarea",
+            label: "TextArea",
+            model: "areavalue",
+      });
+    },
+        addBouttonRadio() {
+                this.schema.fields.push({inputType: "bouttonRadio",
+                label: "bouttonRadio",
+                model: "radiobox",
+      });
+    },
         getCompo(item) {
             if (item.inputType == "textarea") {
                 return InputTextArea
-            } else if(item.inputType == "radiobox") {
+            } else if(item.inputType == "bouttonRadio") {
                 return RadioBox
             }
         }
     }
 }
 </script>
+
+<style>
+  
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 480px;
+    height: 600px;
+    display: inline-flex;
+    margin-top: 35px;
+    
+  }
+
+  .fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
+</style>
--- src/views/formulaire.vue
+++ src/views/formulaire.vue
@@ -1,76 +1,27 @@
 <template>
   <div>
     <FormBuilder :schema="schema" :model="model"></FormBuilder>   <!--- Props from FormBuilder = data() from script -->
-    <FormBuilder :schemaRadio="schemaRadio" :modelRadio="modelRadio"></FormBuilder>
-    <el-card class="box-card">
-  <div slot="header" class="clearfix">
-    <span>All Elements</span>
-  </div>
-  <div v-for="o in 4" :key="o" class="text item" >
-    <el-button type="text" v-on:click="show = !show" >Bouton</el-button>
-  </div>
-</el-card>
   </div>
 </template>
 
 
 <script>
 import FormBuilder from "../components/FormBuilder"
+
+
 export default {
     components: { 
-      FormBuilder
+      FormBuilder,
+    
     },
      data() {
         return {
           schema: {
             fields: 
-            [{
-            inputType: "textarea",
-            label: "TextArea",
-            model: "areavalue"
-            },{
-            inputType: "radiobox",
-            label: "RadioBox",
-            model: "radiovalue"
-            }]
-            },
-          model: {
-            areavalue: "",
-            radiovalue: ""
-          }
-        }
-     }
-}
+            [{}]
+     },
+    }
+  }
+}  
 </script>
 
-<style>
-  .text {
-    font-size: 14px;
-  }
-
-  .item {
-    margin-bottom: 18px;
-  }
-
-  .clearfix:before,
-  .clearfix:after {
-    display: table;
-    content: "";
-  }
-  .clearfix:after {
-    clear: both
-  }
-
-  .box-card {
-    width: 480px;
-    height: 600px;
-  }
-
-  .fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
-
-</style>
