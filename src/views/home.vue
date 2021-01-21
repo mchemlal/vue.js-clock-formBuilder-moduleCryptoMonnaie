31af2d8bd19403df091e80ee5f76ba264b4d677e
seb830 <seb.flouvat@gmail.com>
2021-01-21 16:58:37 +0100

    homepage

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,3 +1,36 @@
 <template>
-    <div></div>
+    <div>
+        <h1 style="margin-top:50px;">Team Boulard</h1>
+        <div class="figure">
+            <figure class="fab">
+                <img src="../assets/Fab2.png"  alt="fab" width="190" height="200"/>
+                <figcaption class="tit1 title">Fabien</figcaption>
+            </figure>
+            <figure class="momo">
+                <img src="../assets/momo.png"  alt="momo" width="190" height="200"/>
+                <figcaption class="tit2 title">Morade</figcaption>
+            </figure>
+            <figure class="fab">
+                <img src="../assets/polo.png"  alt="polo" width="190" height="200"/>
+                <figcaption class="tit3 title">Paul</figcaption>
+            </figure>
+            <figure class="fab">
+                <img src="../assets/Yo.png"  alt="Yo" width="190" height="200"/>
+                <figcaption class="tit4 title">Yoann</figcaption>
+            </figure>
+            <figure class="fab">
+                <img src="../assets/seb.png"  alt="seb" width="190" height="200"/>
+                <figcaption class="tit5 title">Sébastien</figcaption>
+            </figure>
+               
+        </div>
+    </div>
 </template>
+
+<style>
+.figure{
+    display:flex;
+    justify-content:center;
+    flex-flow: row wrap;
+}
+</style>
