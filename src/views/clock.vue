ce28dd373ba8447b39e0c30d867d3f44a11ef0af
seb830 <seb.flouvat@gmail.com>
2021-01-21 12:30:34 +0100

    css clock2

--- src/views/clock.vue
+++ src/views/clock.vue
@@ -1,14 +1,17 @@
 <template>
  <div>
-     <v-clock id="clock" size="300px" border="none" color="#FFFFFF" bg="#4AB7BD" style="margin-top: 100px; margin-bottom: 50px;"></v-clock>
-     <div id="clock-international">
-     <div class="brand">Team Boulard</div>
-     <div style="text-align:center;padding:1em 0;"><h4><a style="text-decoration:none;" href="https://www.zeitverschiebung.net/fr/city/2988507%22%3E"></a><span style="color:gray;">Heure actuelle</span><br />Paris, France</h4> <iframe src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=fr&size=small&timezone=Europe%2FParis" width="100%" height="90" frameborder="0" seamless></iframe></div>
-     <div style="text-align:center;padding:1em 0;"><h4><a style="text-decoration:none;" href="https://www.zeitverschiebung.net/fr/city/2643743%22%3E"></a><span style="color:gray;">Heure actuelle</span><br />London, Royaume-Uni</h4> <iframe src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=fr&size=small&timezone=Europe%2FLondon" width="100%" height="90" frameborder="0" seamless></iframe></div>
-     <div style="text-align:center;padding:1em 0;"><h4><a style="text-decoration:none;" href="https://www.zeitverschiebung.net/fr/city/1816670%22%3E"></a><span style="color:gray;">Heure actuelle</span><br />Beijing, Chine</h4> <iframe src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=fr&size=small&timezone=Asia%2FShanghai" width="100%" height="90" frameborder="0" seamless></iframe></div>
-     <div style="text-align:center;padding:1em 0;"><h4><a style="text-decoration:none;" href="https://www.zeitverschiebung.net/fr/city/5128581%22%3E"></a><span style="color:gray;">Heure actuelle</span><br />New York City, États-Unis (USA)</h4> <iframe src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=fr&size=small&timezone=America%2FNew_York" width="100%" height="90" frameborder="0" seamless></iframe></div>
+    <div>
+        <v-clock id="clock" size="300px" border="none" color="#FFFFFF" bg="#4AB7BD" style="margin-top: 100px; margin-bottom: 50px;"></v-clock>
+        <p class="brand">Team Boulard</p>
      </div>
- </div>
+
+     <div id="clock-international">
+        <div style="text-align:center;padding:1em 0;"><h4><a style="text-decoration:none;" href="https://www.zeitverschiebung.net/fr/city/2988507%22%3E"></a><span style="color:gray;">Heure actuelle</span><br />Paris, France</h4> <iframe src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=fr&size=small&timezone=Europe%2FParis" width="100%" height="90" frameborder="0" seamless></iframe></div>
+        <div style="text-align:center;padding:1em 0;"><h4><a style="text-decoration:none;" href="https://www.zeitverschiebung.net/fr/city/2643743%22%3E"></a><span style="color:gray;">Heure actuelle</span><br />London, Royaume-Uni</h4> <iframe src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=fr&size=small&timezone=Europe%2FLondon" width="100%" height="90" frameborder="0" seamless></iframe></div>
+        <div style="text-align:center;padding:1em 0;"><h4><a style="text-decoration:none;" href="https://www.zeitverschiebung.net/fr/city/1816670%22%3E"></a><span style="color:gray;">Heure actuelle</span><br />Beijing, Chine</h4> <iframe src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=fr&size=small&timezone=Asia%2FShanghai" width="100%" height="90" frameborder="0" seamless></iframe></div>
+        <div style="text-align:center;padding:1em 0;"><h4><a style="text-decoration:none;" href="https://www.zeitverschiebung.net/fr/city/5128581%22%3E"></a><span style="color:gray;">Heure actuelle</span><br />New York City, États-Unis (USA)</h4> <iframe src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=fr&size=small&timezone=America%2FNew_York" width="100%" height="90" frameborder="0" seamless></iframe></div>
+     </div>
+ </div style="margin-bottom:100px;">
 </template>
 
 <script>
@@ -44,16 +47,19 @@ export default {
     flex-direction: row;
     justify-content: center;
     position:relative;
+    margin-top:250px ;
 }
 
 .brand{
     font-size:24px;
     font-style:italic;
     font-weight:bold;
-    position :absolute;
+    text-align:center;
+    /*position :absolute;
     bottom:465px;
-    left: 663px;
+    left: 663px;*/
     z-index: 0;
+    margin-top:-275px;
     opacity: 0.6;
     font-family : "Tangerine";
 }
